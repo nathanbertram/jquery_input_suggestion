2e4e6de71092b200b36f9a880162ca369b985b0f
Nathan Bertram <nbertram@gmail.com>
2011-07-07 23:18:36 -0500

    Probably just re-invented the wheel; but works for what I need it for :)

---
+++ jquery.input_suggestion.js
@@ -0,0 +1,36 @@
+function initInputSuggestion(custom_settings){
+  var default_settings = {
+    input_selctor_param: 'data-suggestion-text',
+    active_class: 'suggestion_text'
+  };
+  var settings = $.extend(true, default_settings, custom_settings);
+    
+  $('input['+settings['input_selctor_param']+']').each(function(){
+    var elem = $(this);
+
+    if(elem.attr('value') == ''){
+      elem.attr('value', elem.attr(settings['input_selctor_param']));
+      elem.addClass(settings['active_class']);
+      elem.bind('focusin', function(){
+        if(elem.hasClass(settings['active_class'])){
+          elem.attr('value', '');
+          elem.removeClass(settings['active_class']);
+        }
+      });
+
+      elem.bind('focusout', function(){
+        if(elem.attr('value') == ''){
+          elem.addClass(settings['active_class']);
+          elem.attr('value', elem.attr(settings['input_selctor_param']));
+        }
+      });
+    }
+  });
+
+  // Clear the default text before the form is submitted
+  $('input['+settings['input_selctor_param']+']').parents().submit(function(){
+    $('input['+settings['input_selctor_param']+']').each(function(){
+      if ($(this).hasClass(settings['active_class'])) this.value = '';
+    });
+  });
+}
